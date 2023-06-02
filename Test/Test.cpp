edaa065720cfa433f74090f56c3aad328c87acdd
Peter Major <axodox@outlook.com>
2023-06-02 10:36:03 +0200

    Performed safety test

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -10,12 +10,7 @@
 #include "Storage/FileStream.h"
 #include "Storage/FileIO.h"
 
-#include "MachineLearning/TextTokenizer.h"
-#include "MachineLearning/TextEncoder.h"
-#include "MachineLearning/StableDiffustionInferer.h"
-#include "MachineLearning/VaeDecoder.h"
-#include "MachineLearning/VaeEncoder.h"
-#include "MachineLearning/TextEmbedder.h"
+#include "MachineLearning/SafetyChecker.h"
 
 using namespace Axodox::Graphics;
 using namespace Axodox::Infrastructure;
@@ -26,135 +21,41 @@ using namespace std;
 using namespace winrt;
 
 
-TextureData ReadPfm(const std::filesystem::path& path)
-{
-  //Create stream
-  file_stream stream{ path, file_mode::read };
-
-  //Read header
-  auto magic = stream.read_line();
-  auto resolution = stream.read_line();
-  uint32_t width, height;
-  sscanf_s(resolution.c_str(), "%u %u", &width, &height);
-
-  auto bias = stream.read_line();
-
-  //Read depth data
-  TextureData result{ width, height, DXGI_FORMAT_R32_FLOAT };
-  stream.read(result.Buffer);
-
-  return result;
-}
-
-void test()
-{
-  std::cout << "Hello World!\n";
-
-  auto textureData = TextureData::FromBuffer(read_file(L"C:\\dev\\ai\\MiDaS\\input\\test.png"));
-  auto depthData = ReadPfm(L"C:\\dev\\ai\\MiDaS\\output\\test-dpt_beit_large_512.pfm");
-
-  auto depthBuffer = depthData.Cast<float>();
-
-  Window window{ L"Hello world!" };
-
-  GraphicsDevice device{};
-  HwndSwapChain swapChain{ device, *window };
-
-  window.Painting(no_revoke, [&](Window*) {
-    swapChain.BackBuffer()->Clear();
-    swapChain.Present();
-    });
-
-  window.Resized(no_revoke, [&](Window*) {
-    swapChain.Resize();
-    });
-
-  window.Show();
-  window.Run();
-}
-
 int main()
 {
   winrt::init_apartment();
 
-  OnnxEnvironment onnxEnvironment{ L"C:/dev/ai/realistic_vision_v1.4-fp16-vram" };
+  OnnxEnvironment onnxEnvironment{ L"D:/dev/realistic_vision_onnx" };
 
-  //OnnxEnvironment onnxEnvironment{ L"D:/dev/Stable-Diffusion-USE_ONNX-FP16/model/safetensors-sd15-fp16" };
-  //OnnxEnvironment onnxEnvironment{ L"D:/dev/Stable-Diffusion-USE_ONNX-FP16/model/safetensors-protogenX53Photorealism_10-fp16" };
-  //OnnxEnvironment onnxEnvironment{ L"C:/dev/StableDiffusion/StableDiffusion" };
-  //OnnxEnvironment onnxEnvironment{ L"D:/dev/stable-diffusion-2-1-fp16-onnx" };
+  SafetyChecker safetyChecker{ onnxEnvironment };
 
-  //Load source image
-  Tensor latentInput;
+  filesystem::path sourceDirectory{ "F:/Images/unpaint" };
+  filesystem::path safeDirectory{ "F:/Images/unpaint/safe" };
+  filesystem::path unsafeDirectory{ "F:/Images/unpaint/unsafe" };
+
+  filesystem::create_directories(safeDirectory);
+  filesystem::create_directories(unsafeDirectory);
+
+  for (auto item : filesystem::directory_iterator{ sourceDirectory })
   {
-    auto pngBuffer = read_file(L"bin/input.png");
-    auto imageTexture = TextureData::FromBuffer(pngBuffer);
-    Tensor imageTensor = Tensor::FromTextureData(imageTexture);
+    if (item.is_directory() || item.path().extension() != ".png") continue;
 
-    VaeEncoder vaeEncoder{ onnxEnvironment };
-    latentInput = vaeEncoder.EncodeVae(imageTensor);
-  }
+    printf("Checking %s...", to_string(item.path().filename().wstring()).c_str());
 
-  //Load mask image
-  Tensor maskInput;
-  {
-    auto pngBuffer = read_file(L"bin/mask.png");
-    auto imageTexture = TextureData::FromBuffer(pngBuffer, TextureImageFormat::Gray8);
-    maskInput = Tensor::FromTextureData(imageTexture);
-  }
-
-  //Create text embeddings
-  Tensor textEmbeddings;
-  {
-    //Encode text
-    TextTokenizer textTokenizer{ onnxEnvironment };
-    TextEncoder textEncoder{ onnxEnvironment };
-
-    auto tokenizedNegativePrompt = textTokenizer.TokenizeText("blurry, render");//textTokenizer.GetUnconditionalTokens();
-    auto encodedNegativePrompt = textEncoder.EncodeText(tokenizedNegativePrompt);
-
-    auto tokenizedPositivePrompt = textTokenizer.TokenizeText("a stag standing in a snowy winter forest at noon, closeup");
-    auto encodedPositivePrompt = textEncoder.EncodeText(tokenizedPositivePrompt);
-
-    textEmbeddings = encodedNegativePrompt.Concat(encodedPositivePrompt);
-  }
-
-  //Run stable diffusion
-  Tensor latentResult;
-  {
-    StableDiffusionInferer stableDiffusion{ onnxEnvironment };
-
-    StableDiffusionOptions options{
-      .StepCount = 20,
-      .BatchSize = 1,
-      .Width = 768,
-      .Height = 768,
-      .Seed = 88,
-      .TextEmbeddings = textEmbeddings,
-      .LatentInput = latentInput,
-      .MaskInput = maskInput,
-      .DenoisingStrength = 0.6f
-    };
-
-    latentResult = stableDiffusion.RunInference(options);
-  }
-
-  //Decode VAE
-  {
-    VaeDecoder vaeDecoder{ onnxEnvironment };
-    auto imageTensor = vaeDecoder.DecodeVae(latentResult);
-
-    auto imageTextures = imageTensor.ToTextureData();
-
-    for (auto i = 0; auto & imageTexture : imageTextures)
+    auto imageBuffer = try_read_file(item.path());
+    auto imageData = TextureData::FromBuffer(imageBuffer);
+    if (safetyChecker.IsSafe(imageData))
     {
-      auto pngBuffer = imageTexture.ToBuffer();
-      write_file(std::format(L"bin/test{}.png", i++), pngBuffer);
+      printf(" Safe.\n");
+      filesystem::copy_file(item.path(), safeDirectory / item.path().filename());
+    }
+    else
+    {
+      printf(" Not safe.\n");
+      filesystem::copy_file(item.path(), unsafeDirectory / item.path().filename());
     }
   }
 
-  //Done
-  printf("done.");
-
+  printf("Done.\n");
   return 0;
 }
