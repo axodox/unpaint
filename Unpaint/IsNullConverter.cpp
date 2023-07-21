77035307d5357fc021f93b971d9bd5570561fcb3
Peter Major <axodox@outlook.com>
2023-07-21 20:05:11 +0200

    Fixed input pane layout

--- Unpaint/IsNullConverter.cpp
+++ Unpaint/IsNullConverter.cpp
@@ -6,16 +6,29 @@ using namespace winrt::Windows::UI::Xaml;
 
 namespace winrt::Unpaint::implementation
 {
+  bool IsNullConverter::IsNegating()
+  {
+    return _isNegating;
+  }
+
+  void IsNullConverter::IsNegating(bool value)
+  {
+    _isNegating = value;
+  }
+
   Windows::Foundation::IInspectable IsNullConverter::Convert(Windows::Foundation::IInspectable const& value, Windows::UI::Xaml::Interop::TypeName const& targetType, Windows::Foundation::IInspectable const& /*parameter*/, hstring const& /*language*/)
   {
+    auto result = value == nullptr;
+    if (_isNegating) result = !result;
+
     if (targetType == xaml_typename<bool>())
     {
-      return box_value(value == nullptr);
+      return box_value(result);
     }
 
     if (targetType == xaml_typename<Visibility>())
     {
-      return box_value(value == nullptr ? Visibility::Visible : Visibility::Collapsed);
+      return box_value(result ? Visibility::Visible : Visibility::Collapsed);
     }
 
     return nullptr;
