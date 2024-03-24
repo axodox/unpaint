18b430fc31b80bfdbcccc809ae9989ed09ff14d0
Peter Major <axodox@outlook.com>
2024-03-24 18:59:35 +0100

    Finish integration

--- Unpaint/OnnxHost.cpp
+++ Unpaint/OnnxHost.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "OnnxHost.h"
 
+using namespace Axodox::Infrastructure;
 using namespace Axodox::MachineLearning::Executors;
 using namespace Axodox::MachineLearning::Sessions;
 using namespace std;
@@ -8,8 +9,10 @@ using namespace std;
 namespace winrt::Unpaint
 {
   OnnxHost::OnnxHost() :
+    _state(dependencies.resolve<UnpaintState>()),
     _environment(make_shared<OnnxEnvironment>()),
-    _executor(make_shared<DmlExecutor>())
+    _executor(make_shared<DmlExecutor>(*_state->AdapterIndex)),
+    _adapterIndexChangedSubscription(_state->AdapterIndex.ValueChanged({ this, &OnnxHost::OnAdapterIndexChanged }))
   { }
 
   const std::shared_ptr<Axodox::MachineLearning::Sessions::OnnxEnvironment>& OnnxHost::Environment() const
@@ -30,5 +33,10 @@ namespace winrt::Unpaint
   Axodox::MachineLearning::Sessions::OnnxSessionParameters OnnxHost::ParametersFromFile(const winrt::Windows::Storage::StorageFile& file) const
   {
     return { _environment, _executor, OnnxModelSource::FromStorageFile(file) };
+  }
+
+  void OnnxHost::OnAdapterIndexChanged(OptionPropertyBase*)
+  {
+    static_pointer_cast<DmlExecutor>(_executor)->ChangeAdapter(*_state->AdapterIndex);
   }
 }
--- Unpaint/OnnxHost.h
+++ Unpaint/OnnxHost.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "pch.h"
+#include "UnpaintState.h"
 
 namespace winrt::Unpaint
 {
@@ -15,7 +15,11 @@ namespace winrt::Unpaint
 		Axodox::MachineLearning::Sessions::OnnxSessionParameters ParametersFromFile(const winrt::Windows::Storage::StorageFile& file) const;
 
 	private:
+		std::shared_ptr<UnpaintState> _state;
 		std::shared_ptr<Axodox::MachineLearning::Sessions::OnnxEnvironment> _environment;
 		std::shared_ptr<Axodox::MachineLearning::Executors::OnnxExecutor> _executor;
+		Axodox::Infrastructure::event_subscription _adapterIndexChangedSubscription;
+
+		void OnAdapterIndexChanged(OptionPropertyBase*);
 	};
 }
